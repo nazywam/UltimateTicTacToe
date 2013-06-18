bb836f9dba1b8ed8f17161359503c25831515501
Michael Von Hell <michal@buka.(none)>
2013-06-18 20:43:14 +0200

    Update

--- Source/Board.hx
+++ Source/Board.hx
@@ -24,28 +24,39 @@ class Board {
 	} 
 	
 	private function drawO(posX : Int,  posY : Int,  size : Int):Void{
-		var mc : flash.display.MovieClip = flash.Lib.current;
+	
 	}
 	private function drawX(posX : Int,  posY : Int,  size : Int):Void{
 		var mc : flash.display.MovieClip = flash.Lib.current;
 		mc.graphics.beginFill( 0xFF0000 );
 		mc.graphics.moveTo( posX, posY );
-		mc.graphics.lineTo( posX+size, posY );
 		mc.graphics.lineTo( posX+size, posY+size );
-		mc.graphics.lineTo( posX, posY+size );
+		mc.graphics.lineTo( posX+size-10, posY+size );
+		mc.graphics.lineTo( posX, posY+10 );
+		mc.graphics.endFill();
+
+		mc.graphics.beginFill( 0xFF0000 );
+		mc.graphics.moveTo( posX, posY+size );
+		mc.graphics.lineTo( posX+size, posY );
+		mc.graphics.lineTo( posX+size, posY+10 );
+		mc.graphics.lineTo( posX+10, posY+size );
 		mc.graphics.endFill();
 	}
+	private function drawLine(posX : Int, posY : Int, width: Int):Void{
+
+	}
 	public function draw():Void{
+		var tileSize : Int = Std.int(sqSize/9);
 		for(x in 0...9){
 			trace(map[x].toString());	
 		}
 		for(y in 0...9){
 			for(x in 0...9){
-				if(map[y][x]==1){
-					drawX(y,x,10);
+				if(map[y][x]==0){
+					drawX(y*tileSize,x*tileSize, tileSize);
 				}
 				else if(map[y][x]==2){
-					drawO(y,x,10);
+					drawO(y*tileSize,x*tileSize, tileSize);
 				}
 			}
 		}			
