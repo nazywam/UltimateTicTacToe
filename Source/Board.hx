0046cbf3a72f293598d665e50ab913b4768c1fc3
Michael Von Hell <michal@buka.(none)>
2013-06-18 17:07:33 +0200

    test

--- Source/Board.hx
+++ Source/Board.hx
@@ -1,6 +1,16 @@
 class Board {
-	var scores : Array<Array<Int>>;
+	var map = Array<Array<Int>>();
 	public function new():Void {
-		
+
+		var line = new Array<Int>();
+		for(x in 0...9) {
+			line.insert(x,0);
+		}
+
+		for(x in 0...9) {
+			map.insert(x, line);
+		}
+
+		//trace(map.toString());	
 	} 
 }
